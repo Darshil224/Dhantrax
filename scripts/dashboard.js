6ac2ff6d2a3c81557b78da60fe3703e517cdea0a
Darshil224 <darshilchitranshi2204@gmail.com>
2026-06-23 19:38:50 +0530

    Render dashboard statistics from transaction data

---
+++ scripts/dashboard.js
@@ -0,0 +1,36 @@
+import { calculateStats } from "../data/transactionsData.js";
+
+export function renderDashboardCardHTML(){
+    const stats = calculateStats();
+    const dashboardCardHTML=`
+    
+    <div class="summary-card">
+            <p class="card-title">Total Balance</p>
+            <p class="card-value">$${stats.totalBalance}</p>
+            <p class="card-change">&uarr; 0% from last month</p>
+          </div>
+          <div class="summary-card">
+            <p class="card-title">Monthly Income</p>
+            <p class="card-value">$${stats.monthlyIncome}</p>
+            <p class="card-change">&uarr; 5% from last month</p>
+          </div>
+          <div class="summary-card">
+            <p class="card-title">Monthly Expenses</p>
+            <p class="card-value">$${stats.monthlyExpense}</p>
+            <p class="card-change">&darr; 2% from last month</p>
+          </div>
+          <div class="summary-card">
+            <p class="card-title">Savings Rate</p>
+            <p class="card-value">${stats.savingsRate.toFixed(1)}%</p>
+            <p class="card-change">% of income</p>
+          </div>
+    
+    `;
+
+    document.querySelector('.card-container')
+    .innerHTML=dashboardCardHTML;
+
+
+}
+
+renderDashboardCardHTML();
